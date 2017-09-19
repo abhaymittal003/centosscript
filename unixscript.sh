45836f1446083217ad4a28f6ef1aa2b1b48c9e4a
Abhay Mittal <30779087+abhaymittal003@users.noreply.github.com>
2017-09-19 15:56:02 +0100

    Update unixscript.sh

--- unixscript.sh
+++ unixscript.sh
@@ -6,6 +6,7 @@ yum install -y gcc libffi-devel python-devel openssl-devel epel-release
 yum install -y python-pip python-wheel
 pip install --upgrade pip
 pip install "azure==2.0.0rc5" msrestazure
+sleep 10
 yum install -y ansible
 pip install packaging
 date > /tmp/test.txt
