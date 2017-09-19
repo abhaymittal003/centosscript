0d5a6138fbe2e86b5a55949102798f0836f91628
Abhay Mittal <30779087+abhaymittal003@users.noreply.github.com>
2017-09-19 16:27:49 +0100

    Update unixscript.sh

--- unixscript.sh
+++ unixscript.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ## Install pre-requisite packages
-yum check-update
-yum install -y gcc libffi-devel python-devel openssl-devel epel-release
-yum install -y python-pip python-wheel
-pip install --upgrade pip
-pip install "azure==2.0.0rc5" msrestazure
+echo "Installation start" > /tmp/install.log
+yum check-update >> /tmp/install.log
+yum install -y gcc libffi-devel python-devel openssl-devel epel-release >> /tmp/install.log
+yum install -y python-pip python-wheel >> /tmp/install.log
+pip install --upgrade pip >> /tmp/install.log
+pip install "azure==2.0.0rc5" msrestazure >> /tmp/install.log
 sleep 5
-yum install -y ansible
-pip install packaging
+yum install -y ansible >> /tmp/install.log
+pip install packaging >> /tmp/install.log
 exit 0
