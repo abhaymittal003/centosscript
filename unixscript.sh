86d2e563df0720c87bcd21238754c45034499dab
Abhay Mittal <30779087+abhaymittal003@users.noreply.github.com>
2017-09-19 15:14:29 +0100

    new file
    new file

---
+++ unixscript.sh
@@ -0,0 +1,5 @@
+## Install pre-requisite packages
+sudo yum check-update; 
+sudo yum install -y gcc libffi-devel python-devel openssl-devel epel-release;
+sudo yum install -y python-pip python-wheel
+# completed
