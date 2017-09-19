4d9252c00d64eb23c873456866136ccb02f17521
Abhay Mittal <30779087+abhaymittal003@users.noreply.github.com>
2017-09-19 16:28:32 +0100

    Update unixscript.sh

--- unixscript.sh
+++ unixscript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ## Install pre-requisite packages
-echo "Installation start" > /tmp/install.log
+date > /tmp/install.log
+echo "Installation start"  >> /tmp/install.log
 yum check-update >> /tmp/install.log
 yum install -y gcc libffi-devel python-devel openssl-devel epel-release >> /tmp/install.log
 yum install -y python-pip python-wheel >> /tmp/install.log
